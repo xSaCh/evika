38f2f924115f77cb97d04686d950ad9206ff4aa2
xSaCh <samarthchauhan7@gmail.com>
2024-10-22 23:02:40 +0530

    Flutter setup

---
+++ lib/screens/base_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BaseScreen extends StatefulWidget {
+  const BaseScreen({super.key});
+
+  @override
+  State<BaseScreen> createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
