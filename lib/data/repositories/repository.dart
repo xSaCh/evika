ee4c9da342feed44ba761604f1ffea723daee5f8
xSaCh <samarthchauhan7@gmail.com>
2024-10-25 00:01:08 +0530

    Repo changes

--- lib/data/repositories/repository.dart
+++ lib/data/repositories/repository.dart
@@ -11,12 +11,15 @@ import 'package:evika/data/repositories/local_repository.dart';
 class Repository {
   final Api api;
   final LocalRepository localRepo;
+  LoginUser? loggedUser;
+
   Repository(this.api, this.localRepo);
 
   Future<LoginUser?> loginUser(String email, String password) async {
     final res = await api.loginUser(email, password);
 
     if (res.$2 != "") localRepo.setToken(res.$2);
+    loggedUser = res.$1;
     return res.$1;
   }
 
@@ -34,6 +37,20 @@ class Repository {
     if (eventRes.$1.isNotEmpty) localRepo.setEvents(eventRes.$1);
 
     return eventRes;
+    File f = File("/home/samarth/events.json");
+    final data = jsonDecode(f.readAsStringSync()) as Map<String, dynamic>;
+    final eventsMap = data['data']['events'];
+    List<Event> newEvent =
+        await eventsMap.map<Event>((event) => Event.fromMap(event)).toList();
+
+    await Future.delayed(Duration(seconds: 2));
+    // throw UnimplementedError();
+    for (var e in newEvent) {
+      final intr = localRepo.getEventInteraction(e.id);
+      if (intr != null) e.setInteractionFields(intr);
+    }
+    if (newEvent.isNotEmpty) localRepo.setEvents(newEvent);
+    return (newEvent, 6);
   }
 
   Future<(List<Event>, int)> searchEvents(String query, {int page = 1}) async {
@@ -46,10 +63,24 @@ class Repository {
       final intr = localRepo.getEventInteraction(e.id);
       if (intr != null) e.setInteractionFields(intr);
     }
-    // Cached lastest events to local DB
-    if (eventRes.$1.isNotEmpty) localRepo.setEvents(eventRes.$1);
 
     return eventRes;
+    File f = File("/home/samarth/events.json");
+    final data = jsonDecode(f.readAsStringSync()) as Map<String, dynamic>;
+    final eventsMap = data['data']['events'];
+    List<Event> newEvent =
+        await eventsMap.map<Event>((event) => Event.fromMap(event)).toList();
+
+    for (var e in newEvent) {
+      final intr = localRepo.getEventInteraction(e.id);
+      if (intr != null) e.setInteractionFields(intr);
+    }
+
+    newEvent = newEvent
+        .where((e) => e.title.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+
+    return (newEvent, 6);
   }
 
   Future<void> setEventInteraction(Event event) async {
